97f3aa9f3b6dcc50bbf98539044e2c69f39dcd4f
HorCsa <horvath.csaba.82@gmail.com>
2021-02-23 22:47:40 +0100

    initial commit

---
+++ projects/hcs/src/public-api.ts
@@ -0,0 +1,8 @@
+/*
+ * Public API Surface of hcs
+ */
+
+// export * from './lib/hcs.service';
+// export * from './lib/hcs.component';
+// export * from './lib/hcs.module';
+export * from './lib/components';
